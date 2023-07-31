0cbb19fad821c14070b6df82642cb8f24998a70b
0xxhaisenberg <github@0xhaisenberg.33mail.com>
2023-07-31 21:23:04 +0000

    modified

--- airflow/dags/ingest_listing.py
+++ airflow/dags/ingest_listing.py
@@ -13,6 +13,7 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExte
 import pyarrow.csv as pv
 import pyarrow.parquet as pq
 
+
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
 
@@ -21,19 +22,16 @@ BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'airbnb_amsterdam_june')
 batch_date = '2023-06-05'
 base_url = 'http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam'
 
-listings = f"{base_url}/{batch_date}/data/listings.csv.gz"
+dataset_url = f"{base_url}/{batch_date}/data/listings.csv.gz"
+dataset_file = 'listings.csv.gz'
+parquet_file = dataset_file.replace('csv.gz', '.parquet')
 
 
 
 
-def to_parquet():
-
-
-    # parquetize and save to local
-    data_dir = f'airbnb/amsterdam'
-    Path(data_dir).mkdir(parents=True, exist_ok=True)
-    path = Path(f'{data_dir}/{____}.parquet')
-    df.to_parquet(path, compression='gzip')
+def format_to_parquet(src_file):
+    table = pv.read_csv(src_file)
+    pq.write_table(table, src_file.replace('.csv', '.parquet'))
 
 
 
@@ -59,3 +57,58 @@ default_args = {
     "depends_on_past": False,
     "retries": 1,
 }
+
+# NOTE: DAG declaration - using a Context Manager (an implicit way)
+with DAG(
+    dag_id="ingest_listing_data",
+    schedule_interval="0 6 2 * *",
+    start_date=datetime(2019, 1, 1),
+    default_args=default_args,
+    catchup=True,
+    max_active_runs=3,
+    tags=['dtc-de-homework'],
+) as dag:
+
+    download_dataset_task = BashOperator(
+        task_id="download_dataset_task",
+        bash_command=f"wget {base_url}/{batch_date}/data/{dataset_file} > {path_to_local_home}/"
+    )
+
+    format_to_parquet_task = PythonOperator(
+    task_id="format_to_parquet_task",
+    python_callable=format_to_parquet,
+    op_kwargs={
+        "src_file": f"{path_to_local_home}/{dataset_file}",
+        },
+    )
+
+    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
+    local_to_gcs_task = PythonOperator(
+        task_id="local_to_gcs_task",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"airbnb_amsterdam/{parquet_file}",
+            "local_file": f"{path_to_local_home}/{parquet_file}",
+        },
+    )
+
+    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
+        task_id="bigquery_external_table_task",
+        table_resource={
+            "tableReference": {
+                "projectId": PROJECT_ID,
+                "datasetId": BIGQUERY_DATASET,
+                "tableId": "listings",
+            },
+            "externalDataConfiguration": {
+                "sourceFormat": "PARQUET",
+                "sourceUris": [f"gs://{BUCKET}/airbnb_amsterdam/{parquet_file}"],
+            },
+        },
+    )
+
+    remove_files_task = BashOperator(
+        task_id="remove_files_task",
+        bash_command=f"rm {path_to_local_home}/{dataset_file} {path_to_local_home}/{parquet_file}"
+    )
