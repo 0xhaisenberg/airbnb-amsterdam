424e692d3a4bd925604c40c573e4076751e690b6
0xxhaisenberg <github@0xhaisenberg.33mail.com>
2023-08-01 15:40:35 +0000

    modified

--- airflow/dags/ingest_listing.py
+++ airflow/dags/ingest_listing.py
@@ -7,31 +7,34 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 from datetime import datetime
+from datetime import timedelta
 
 from google.cloud import storage
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
-import pyarrow.csv as pv
-import pyarrow.parquet as pq
+import pandas as pd
+import pyarrow
 
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
 BUCKET = os.environ.get("GCP_GCS_BUCKET")
-
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'airbnb_amsterdam_june')
 
 batch_date = '2023-06-05'
 base_url = 'http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam'
 
+# path_to_dir = os.getcwd()
+
+path_to_file = '/home/haisenberg/airbnb-amsterdam/airflow/data'
 dataset_url = f"{base_url}/{batch_date}/data/listings.csv.gz"
 dataset_file = 'listings.csv.gz'
-parquet_file = dataset_file.replace('csv.gz', '.parquet')
+parquet_file = dataset_file.replace('csv.gz', 'parquet')
 
 
 
 
 def format_to_parquet(src_file):
-    table = pv.read_csv(src_file)
-    pq.write_table(table, src_file.replace('.csv', '.parquet'))
+    df = pd.read_csv(src_file)
+    return df.to_parquet(f'{parquet_file}', compression='gzip')
 
 
 
@@ -52,6 +55,7 @@ def upload_to_gcs(bucket, object_name, local_file):
     blob.upload_from_filename(local_file)
 
 
+
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
@@ -61,40 +65,39 @@ default_args = {
 # NOTE: DAG declaration - using a Context Manager (an implicit way)
 with DAG(
     dag_id="ingest_listing_data",
-    schedule_interval="0 6 2 * *",
-    start_date=datetime(2019, 1, 1),
+    schedule_interval=timedelta(days=90),
+    start_date=days_ago(1),
     default_args=default_args,
     catchup=True,
     max_active_runs=3,
-    tags=['dtc-de-homework'],
+    tags=['airbnb-amsterdam-june'],
 ) as dag:
 
     download_dataset_task = BashOperator(
         task_id="download_dataset_task",
-        bash_command=f"wget {base_url}/{batch_date}/data/{dataset_file} > {path_to_local_home}/"
+        bash_command=f"wget -P {path_to_file}/ {base_url}/{batch_date}/data/{dataset_file}"
     )
 
     format_to_parquet_task = PythonOperator(
     task_id="format_to_parquet_task",
     python_callable=format_to_parquet,
     op_kwargs={
-        "src_file": f"{path_to_local_home}/{dataset_file}",
+        "src_file": f"{path_to_file}/{dataset_file}",
         },
     )
 
-    # TODO: Homework - research and try XCOM to communicate output values between 2 tasks/operators
     local_to_gcs_task = PythonOperator(
         task_id="local_to_gcs_task",
         python_callable=upload_to_gcs,
         op_kwargs={
             "bucket": BUCKET,
-            "object_name": f"airbnb_amsterdam/{parquet_file}",
-            "local_file": f"{path_to_local_home}/{parquet_file}",
+            "object_name": f"airbnb_amsterdam/data/{parquet_file}",
+            "local_file": f"{path_to_file}/{parquet_file}",
         },
     )
 
-    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
-        task_id="bigquery_external_table_task",
+    bigquery_table_task = BigQueryCreateExternalTableOperator(
+        task_id="bigquery_table_task",
         table_resource={
             "tableReference": {
                 "projectId": PROJECT_ID,
@@ -110,5 +113,7 @@ with DAG(
 
     remove_files_task = BashOperator(
         task_id="remove_files_task",
-        bash_command=f"rm {path_to_local_home}/{dataset_file} {path_to_local_home}/{parquet_file}"
+        bash_command=f"rm {path_to_file}/{dataset_file} {path_to_file}/{parquet_file}"
     )
+
+    download_dataset_task >> format_to_parquet_task >> local_to_gcs_task >> bigquery_table_task >> remove_files_task
