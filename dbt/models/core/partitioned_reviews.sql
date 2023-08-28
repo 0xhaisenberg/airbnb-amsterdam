873e5acee9d38d3b58a5f03e6f0b6430e613be42
Kosiso Aniebue <kosymcqueen@gmail.com>
2023-08-28 22:51:09 +0000

    new  model based on reviews data

--- dbt/models/core/partitioned_reviews.sql
+++ dbt/models/core/partitioned_reviews.sql
@@ -3,7 +3,17 @@
     materialized='table',
     partition_by={
       "field": "date",
-      "data_type": "timestamp",
+      "data_type": "date",
       "granularity": "day"
     }
 )}}
+
+select 
+        listing_id,
+        id,
+        date,
+        reviewer_id,
+        reviewer_name,
+        comments
+
+        from {{ ref('base_reviews')}}
